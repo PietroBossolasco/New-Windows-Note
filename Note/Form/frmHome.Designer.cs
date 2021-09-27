fa7d3b145307c7d27b88b1068d4a476cfec115a7
PietroBossolasco <p.bossolasco.1715@vallauri.edu>
2021-09-27 19:40:15 +0200

    New graphic

--- Note/Form/frmHome.Designer.cs
+++ Note/Form/frmHome.Designer.cs
@@ -29,37 +29,69 @@ namespace Note
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnNewFile = new System.Windows.Forms.Button();
+            this.pnlNewDoc = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlNewDoc.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // btnNewFile
+            // pnlNewDoc
             // 
-            this.btnNewFile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnNewFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnNewFile.Location = new System.Drawing.Point(746, -2);
-            this.btnNewFile.Name = "btnNewFile";
-            this.btnNewFile.Size = new System.Drawing.Size(55, 55);
-            this.btnNewFile.TabIndex = 0;
-            this.btnNewFile.Text = "+";
-            this.btnNewFile.UseVisualStyleBackColor = true;
-            this.btnNewFile.Click += new System.EventHandler(this.btnNewFile_Click);
+            this.pnlNewDoc.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlNewDoc.Controls.Add(this.pictureBox1);
+            this.pnlNewDoc.Controls.Add(this.label1);
+            this.pnlNewDoc.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pnlNewDoc.Location = new System.Drawing.Point(12, 62);
+            this.pnlNewDoc.Name = "pnlNewDoc";
+            this.pnlNewDoc.Size = new System.Drawing.Size(145, 181);
+            this.pnlNewDoc.TabIndex = 1;
+            this.pnlNewDoc.Click += new System.EventHandler(this.pnlNewDoc_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = global::Note.Properties.Resources.add;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(137, 150);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pnlNewDoc_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 156);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nuovo documento";
+            this.label1.Click += new System.EventHandler(this.pnlNewDoc_Click);
             // 
             // frmHome
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnNewFile);
+            this.Controls.Add(this.pnlNewDoc);
             this.Name = "frmHome";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.frmHome_Load);
+            this.pnlNewDoc.ResumeLayout(false);
+            this.pnlNewDoc.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnNewFile;
+        private System.Windows.Forms.Panel pnlNewDoc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
